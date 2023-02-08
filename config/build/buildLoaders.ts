1c11b03465116fafb4489885f9d71abb11011000
unknown <lakeeri13@yansex.ru>
2023-02-08 15:11:15 +0300

    config directory

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,36 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export function buildloaders({ isDev }: BuildOptions):webpack.RuleSetRule[]{
+
+  const tsloader =  {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  const cssloader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+            localIdentName: isDev
+            ? "[path][name]__[local]--[hash:base64:5]"
+            :"[hash:base64:8]"
+          },
+        }
+      },
+      "sass-loader",
+    ],
+  };
+
+    return  [
+      tsloader,
+      cssloader
+      ]
+}
