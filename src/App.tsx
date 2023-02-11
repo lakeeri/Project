e2a4ae7ed8a0f2e2ca3b158f12896e6541b86568
unknown <lakeeri13@yansex.ru>
2023-02-11 20:03:07 +0300

    className helper

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync';
 import { MainPageAsync } from './pages/MainPage/MainPageAsync';
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 
 
 export default function App() {
 const {theme, toggleTheme } = useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {} , [theme])}>
           <button onClick={toggleTheme}>Тема</button>
            <Link to="/">Главная страница</Link>
            <Link to="/about">Информация о сайте</Link>
