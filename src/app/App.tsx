ba92e2ad178e308534168047589cb2fa8470de0d
unknown <lakeeri13@yansex.ru>
2023-02-12 10:04:57 +0300

    Navbar, AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 
 
@@ -11,10 +11,9 @@ export default function App() {
 const {theme, toggleTheme } = useTheme();
     return (
         <div className={classNames('app', {} , [theme])}>
+          <Navbar/>
+          <AppRouter />
           <button onClick={toggleTheme}>Тема</button>
-           <Link to="/">Главная страница</Link>
-           <Link to="/about">Информация о сайте</Link>
-           <AppRouter />
          </div>
     )
 }
