af3138fde2d7f0a1e4b2d411fdd5359dae86d309
unknown <lakeeri13@yansex.ru>
2023-02-23 17:55:01 +0300

    jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -43,5 +44,6 @@ module.exports = {
         'no-undef': 'warn',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
 };
---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,39 @@
+import { classNames } from './classNames';
+
+describe('className', () => {
+    test('with only first param', () => {
+        expect(classNames('someClasses')).toBe('someClasses');
+    });
+    test('with additional class', () => {
+        const example = classNames('someClasses', {}, ['class1', 'class2']);
+        const expected = 'someClasses class1 class2';
+        expect(example).toBe(expected);
+    });
+    test('with mods', () => {
+        const example = classNames(
+            'someClasses',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        );
+        const expected = 'someClasses class1 class2 hovered scrollable';
+        expect(example).toBe(expected);
+    });
+    test('with mods false', () => {
+        const example = classNames(
+            'someClasses',
+            { hovered: true, scrollable: false },
+            ['class1', 'class2'],
+        );
+        const expected = 'someClasses class1 class2 hovered';
+        expect(example).toBe(expected);
+    });
+    test('with mods undefined', () => {
+        const example = classNames(
+            'someClasses',
+            { hovered: true, scrollable: false },
+            ['class1', 'class2'],
+        );
+        const expected = 'someClasses class1 class2 hovered';
+        expect(example).toBe(expected);
+    });
+});
