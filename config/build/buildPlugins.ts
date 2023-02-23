2bfb752a9d52ea918f38ffad073a2b36fa1f0956
unknown <lakeeri13@yansex.ru>
2023-02-23 14:30:10 +0300

    ReactRefreshWebpackPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,10 +1,12 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins({paths, isDev }:BuildOptions):webpack.WebpackPluginInstance[] {
-    return [
+    
+    const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html
         }),
@@ -15,6 +17,13 @@ export function buildPlugins({paths, isDev }:BuildOptions):webpack.WebpackPlugin
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
     ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin()),
+        plugins.push(new ReactRefreshWebpackPlugin())
+    }
+
+    return plugins
 }
