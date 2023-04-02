c83885a2031972e2ab857e813686431435164775
unknown <lakeeri13@yansex.ru>
2023-04-02 21:10:13 +0300

    storybook

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
@@ -6,15 +7,18 @@ interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }:NavbarProps) => (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-                Главная страница
-            </AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-                Информация о сайте
-            </AppLink>
+export const Navbar = ({ className }:NavbarProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                    {t('Главная страница')}
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+                    {t('О сайте')}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
